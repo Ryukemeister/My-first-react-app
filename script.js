4c0051bc07368ac184a5e3c3416dbaa24e403746
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-01 20:23:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+      <li>Five</li>
+    </ul>
+    <h1>HI</h1>
+    <h1>HI</h1>
+  </div>,
+  document.getElementById("root")
+);
