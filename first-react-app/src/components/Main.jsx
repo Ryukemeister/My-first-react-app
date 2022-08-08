f8ec230436141cd8792c9208f9886dacc1feb883
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-08 16:19:58 +0530

    Creating custom components for navbar and main page

--- first-react-app/src/components/Main.jsx
+++ first-react-app/src/components/Main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Main.css";
 
 function Main() {
   return (
-    <main>
-      <h1>Main goes here</h1>
+    <main className="main">
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="facts-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintianed by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
   );
 }
--- first-react-app/src/components/Navbar.jsx
+++ first-react-app/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>Navbar goes here</h1>
+    <nav className="nav">
+      <div className="react-logo">
+        <img src="./src/assets/react.svg"></img>
+        <h1 className="title">ReactFacts</h1>
+      </div>
+      <h3 className="desc">React Course - Project 1</h3>
     </nav>
   );
 }
